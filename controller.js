4016baf9d0cc5f072d85da9969798912aa6094ea
syahrulamri11 <syahrul11aemry@gmail.com>
2023-11-29 21:33:32 +0800

    berhasil menampilkan data makanan

--- controller.js
+++ controller.js
@@ -7,3 +7,13 @@ exports.index = function(req, res) {
   response.ok('REST API berhasil berjalan!', res);
 };
 
+//menampilkan semua data API
+exports.allDataApi = function(req, res) {
+  connection.query('SELECT * FROM culinary', function(error, rows, fields) {
+    if(error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res)
+    }
+  });
+};
